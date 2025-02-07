e3ea463d8886e0a57ac5840c8118267e9c58c11c
llDeRuNll <eduardprudiys@gmail.com>
2025-02-07 07:49:48 -0500

    eduard

--- js/module-01/lesson-1-2.js
+++ js/module-01/lesson-1-2.js
@@ -25,3 +25,33 @@
 // console.log(5 >= 3);
 // console.log(5 >= 5);
 // console.log(3 >= 4);
+
+//* Не суворе рівенство (==)
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// console.log(undefined === null);
+
+// console.log('123' === 123);
+// console.log(true == '1');
+// console.log(4 === 5 - true);
+
+//* Суворе рівенство (===)
+// console.log('123' === 123);
+// console.log(null === null);
+// console.log(2 + 2 === 4);
+// console.log(0 === -0);
+//console.log(undefined === undefined);
+
+//* Не дорівнює (!=)
+// console.log('4' != 4);
+
+//* Суворе не дорівнює (!==)
+//console.log(NaN !== NaN);
+
+//let string = "1,58px";
+//string = string.replace(',' , '.');
+//console.log(Number.parseFloat(string))
+
+//console.log(3 > 2 > 1);
