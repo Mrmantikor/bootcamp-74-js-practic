5349c3b837f3cf8775b0591f95b225c62d1ba056
Urban <urban.mutagen@gmail.com>
2025-02-10 14:35:25 +0200

    add lesson 2-1 urban

---
+++ js/module-02/lesson-1-1.js
@@ -0,0 +1,29 @@
+//? Логічні оператори
+// console.log(true && 3);
+// console.log(false && 3);
+// console.log(true && 4 && "kiwi");
+// console.log((true && 0) || "kiwi");
+// console.log(true || 3);
+// console.log(true || 3 || 4);
+// console.log(true || false || 7);
+// console.log(null || 2 || undefined);
+// console.log((1 && null && 2) >= 0);
+// console.log(null || (2 && 3) || 4);
+
+//? Перетворення типів
+// console.log(2 + 5 + "" + 1 + 0);
+// console.log("" + 1 - 0);
+// console.log(true + false);
+// console.log(6 / Number.parseInt("3px"));
+// console.log("2" * "3");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 + 5);
+// console.log("4" - 2);
+// console.log("4px" - 2);
+// console.log(7 / 0);
+// console.log("  -9  " + 5);
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log("" + 1);
+// console.log(3 > 2 > 1);
+// console.log(3 > 2 && 2 > 1);
