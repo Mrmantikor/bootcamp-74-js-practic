d8b636ecc6293edb6f3f24d4a1f2c10392fc85ea
Maksim Peskov <hasagiluv@gmail.com>
2025-02-12 14:59:39 +0200

    compitetask2

--- js/module-01-02/lesson-1.js
+++ js/module-01-02/lesson-1.js
@@ -4,7 +4,28 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
 
-const namber = 70;
-const hours = String(Math.floor(namber / 60)).padStart(2,0);
-const minutes = String(namber % 60).padStart(2, 0);
-console.log(`${hours}:${minutes}`)
+// const namber = 70;
+// const hours = String(Math.floor(namber / 60)).padStart(2,0);
+// const minutes = String(namber % 60).padStart(2, 0);
+// console.log(`${hours}:${minutes}`)
+
+// const languagesStr = "CSS, HTML, CSS, JS, JS, PHP, React, Vue, Node.js, C, C++";
+// let uniqueLetters = "";
+
+// for (let i = 0; i < languagesStr.length; i += 1) {
+//   if (uniqueLetters.includes(languagesStr[i])) {
+//     continue;
+//   }
+//   uniqueLetters += languagesStr[i];
+// }
+// for (let i = 0; i < languagesStr.length; i += 1) {
+//   if (!uniqueLetters.includes(languagesStr[i])) {
+//     uniqueLetters += languagesStr[i];
+//   }
+// }
+// for (let i = 0; i < languagesStr.length; i += 1) {
+//   if (i === languagesStr.indexOf(languagesStr[i])) {
+//     uniqueLetters += languagesStr[i];
+//   }
+// }
+// console.log(uniqueLetters);
