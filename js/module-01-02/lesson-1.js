4b6b3fbe5e0ac411668aab5b81e251c208263f69
Pereverza Tetiana <tanyusha.vzletka82@gmail.com>
2025-02-12 14:33:27 +0200

    padstart

--- js/module-01-02/lesson-1.js
+++ js/module-01-02/lesson-1.js
@@ -3,6 +3,7 @@
 //рядок у форматі годин і хвилин
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
+
 const namber = 70;
 const hours = String(Math.floor(namber / 60)).padStart(2,0);
 const minutes = String(namber % 60).padStart(2, 0);
