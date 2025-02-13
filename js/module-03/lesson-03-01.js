c77767e4c905ea892c5d05b8d3a1ff0bc2fb3885
BakelD <bakeld85@gmail.com>
2025-02-13 13:43:53 +0100

    Add 2 options

--- js/module-03/lesson-03-01.js
+++ js/module-03/lesson-03-01.js
@@ -4,16 +4,50 @@
 // Видалить перший елемент масиву і виведіть його в консоль.
 // Вставте «Реп» і «Реггі» на початок масиву.
 
-const styles = ["Джас", "Блюз"];
+// const styles = ["Джас", "Блюз"];
 
-styles.push("Рок-н-рол");
+// styles.push("Рок-н-рол");
 
-const index = styles.indexOf("Блюз");
+// const index = styles.indexOf("Блюз");
 
-// styles[index] = "Класика";
+// // styles[index] = "Класика";
 
-const deleteElement = styles.shift();
+// const deleteElement = styles.shift();
 
-styles.unshift("«Реп»", "Реггі");
+// styles.unshift("«Реп»", "Реггі");
 
-console.log(styles);
+// console.log(styles);
+
+/*
+Створити функцію multiples(m, n), яка повертає масив перших m кратних дійсного числа n. Вважати, що m - натуральне число.
+
+Наприклад.
+multiples(3, 5) повинна повернути [5, 10, 15]
+*/
+
+// function multiples(m, n) {
+//   const arr = [];
+//   let startNum = n;
+
+//   for (let i = 0; i < m; i += 1) {
+//     arr.push(startNum);
+
+//     startNum += n;
+//   }
+
+//   return arr;
+// }
+
+// option 2
+function multiples(m, n) {
+  const arr = [];
+
+  for (let i = 1; i <= m; i += 1) {
+    arr.push(n * i);
+  }
+
+  return arr;
+}
+
+// multiples(3, 5);
+console.log(multiples(4, 4));
