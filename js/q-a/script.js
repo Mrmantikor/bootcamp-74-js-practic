441b33674a081fbfecc0549653d1a05ea3bfd31c
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-21 19:23:57 +0200

    v

--- js/q-a/script.js
+++ js/q-a/script.js
@@ -49,3 +49,72 @@
 // console.log(arr);
 
 // -------------------------------------------------------------
+
+const vehicles = [
+  { make: 'Honda', model: 'CR-V', type: 'suv', amount: 14, price: 24045, onSale: true },
+  { make: 'Honda', model: 'Accord', type: 'sedan', amount: 2, price: 22455, onSale: true },
+  { make: 'Mazda', model: 'Mazda 6', type: 'sedan', amount: 8, price: 24195, onSale: false },
+  { make: 'Mazda', model: 'CX-9', type: 'suv', amount: 7, price: 31520, onSale: true },
+  { make: 'Toyota', model: '4Runner', type: 'suv', amount: 19, price: 34210, onSale: false },
+  { make: 'Toyota', model: 'Sequoia', type: 'suv', amount: 16, price: 45560, onSale: false },
+  { make: 'Toyota', model: 'Tacoma', type: 'truck', amount: 4, price: 24320, onSale: true },
+  { make: 'Ford', model: 'F-150', type: 'truck', amount: 11, price: 27110, onSale: true },
+  { make: 'Ford', model: 'Fusion', type: 'sedan', amount: 13, price: 22120, onSale: true },
+  { make: 'Ford', model: 'Explorer', type: 'suv', amount: 6, price: 31660, onSale: false },
+];
+
+/*
+1.Реалізувати фільтер за властивістю amount і отримати тільки назву моделі
+*/
+/*
+const getAvailableCarNames = (cars, amountThreshold) => {
+  return cars
+    .filter(el => {
+      return el.amount > amountThreshold;
+    })
+    .map(car => {
+      return car.model;
+    });
+};
+
+console.table(getAvailableCarNames(vehicles, 12));
+*/
+/* Неявне повернення
+const getAvailableCarNames = (cars, amountThreshold) => {
+  return cars.filter(el => el.amount > amountThreshold).map(car => car.model);
+};
+
+console.table(getAvailableCarNames(vehicles, 12));
+*/
+
+/*var-1
+const getAvailableCarNames = (cars, amountThreshold) => {
+  return cars.reduce((acc, el) => {
+    if (el.amount > amountThreshold) {
+      acc.push(el.model);
+    }
+    return acc;
+  }, []);
+};
+
+console.table(getAvailableCarNames(vehicles, 12));
+*/
+/*var-2
+const getAvailableCarNames = (cars, amountThreshold) => {
+  return cars.reduce((acc, el) => {
+    if (el.amount > amountThreshold) {
+      //   acc.push(el.model);
+      return [...acc, el.model];
+    }
+    return acc;
+  }, []);
+};
+
+console.table(getAvailableCarNames(vehicles, 12));
+*/
+
+const getAvailableCarNames = (cars, amountThreshold) => {
+  return cars.reduce((acc, el) => (el.amount > amountThreshold ? [...acc, el.model] : acc), []);
+};
+
+console.table(getAvailableCarNames(vehicles, 12));
