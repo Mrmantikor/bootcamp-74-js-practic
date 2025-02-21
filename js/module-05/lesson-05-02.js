95d4f7ac497f151948edbcfa4db80879b53d0e81
Nikita Naskalnyi <niknnm06@gmail.com>
2025-02-21 15:02:03 +0100

    add task

--- js/module-05/lesson-05-02.js
+++ js/module-05/lesson-05-02.js
@@ -48,101 +48,135 @@
 // console.log(findElement.length !==0);
 // console.log(Boolean(findElement.length));
 
-const people = [
-    {
-      name: 'Alex',
-      know: ['Alex', 'Jhon'],
-    },
-    {
-      name: 'Eva',
-      know: ['Alex', 'Jhon'],
-    },
-    {
-      name: 'Ivan',
-      know: ['Jhon', 'Eva'],
-    },
-    {
-      name: 'Jhon',
-      know: [],
-    },
-  ];
-  //нарцис  'Jhon'
+// const people = [
+//     {
+//       name: 'Alex',
+//       know: ['Alex', 'Jhon'],
+//     },
+//     {
+//       name: 'Eva',
+//       know: ['Alex', 'Jhon'],
+//     },
+//     {
+//       name: 'Ivan',
+//       know: ['Jhon', 'Eva'],
+//     },
+//     {
+//       name: 'Jhon',
+//       know: [],
+//     },
+//   ];
+//   //нарцис  'Jhon'
   
-  const people2 = [
-    {
-      name: 'Alex',
-      know: ['Alex', 'Jhon'],
-    },
-    {
-      name: 'Jhon',
-      know: [],
-    },
-    {
-      name: 'Eva',
-      know: [],
-    },
-    {
-      name: 'Ivan',
-      know: ['Jhon', 'Eva'],
-    },
-  ];
-  //немає нарциса'
-  const people3 = [
-    {
-      name: 'Alex',
-      know: ['Alex', 'Eva'],
-    },
-    {
-      name: 'Jhon',
-      know: [],
-    },
-    {
-      name: 'Eva',
-      know: ['Alex', 'Jhon'],
-    },
-    {
-      name: 'Ivan',
-      know: ['Jhon', 'Eva'],
-    },
-  ];
-  //немає нарциса
+//   const people2 = [
+//     {
+//       name: 'Alex',
+//       know: ['Alex', 'Jhon'],
+//     },
+//     {
+//       name: 'Jhon',
+//       know: [],
+//     },
+//     {
+//       name: 'Eva',
+//       know: [],
+//     },
+//     {
+//       name: 'Ivan',
+//       know: ['Jhon', 'Eva'],
+//     },
+//   ];
+//   //немає нарциса'
+//   const people3 = [
+//     {
+//       name: 'Alex',
+//       know: ['Alex', 'Eva'],
+//     },
+//     {
+//       name: 'Jhon',
+//       know: [],
+//     },
+//     {
+//       name: 'Eva',
+//       know: ['Alex', 'Jhon'],
+//     },
+//     {
+//       name: 'Ivan',
+//       know: ['Jhon', 'Eva'],
+//     },
+//   ];
+//   //немає нарциса
   
-  const people4 = [
-    {
-      name: 'Alex',
-      know: ['Alex', 'Jhon'],
-    },
-    {
-      name: 'Jhon',
-      know: ['Eva'],
-    },
-    {
-      name: 'Eva',
-      know: ['Alex', 'Jhon'],
-    },
-    {
-      name: 'Ivan',
-      know: ['Jhon', 'Eva'],
-    },
-  ];
-  //немає нарциса'
-  //Нарциса знають всі, нарцис незнає нікого
+//   const people4 = [
+//     {
+//       name: 'Alex',
+//       know: ['Alex', 'Jhon'],
+//     },
+//     {
+//       name: 'Jhon',
+//       know: ['Eva'],
+//     },
+//     {
+//       name: 'Eva',
+//       know: ['Alex', 'Jhon'],
+//     },
+//     {
+//       name: 'Ivan',
+//       know: ['Jhon', 'Eva'],
+//     },
+//   ];
+//   //немає нарциса'
+//   //Нарциса знають всі, нарцис незнає нікого
 
-function findNar(array) {
+// function findNar(array) {
 
-    const narzis = array.find(el => el.know.length === 0);
+//     const narzis = array.find(el => el.know.length === 0);
 
-    if (!narzis) {
-        return `немає нарциса`;
-    }
+//     if (!narzis) {
+//         return `немає нарциса`;
+//     }
    
-    const allKnow = array.every(el => {
-        if (el.name === narzis.name) return true;
-        return el.know.includes(narzis.name);
-    });
-    return allKnow ? `Нарицис ${narzis.name}`: `Немає нарциса`;
+//     const allKnow = array.every(el => {
+//         if (el.name === narzis.name) return true;
+//         return el.know.includes(narzis.name);
+//     });
+//     return allKnow ? `Нарицис ${narzis.name}`: `Немає нарциса`;
+// }
+
+// console.log(findNar(people));
+
+//  Дано масив чисел [2, 4, 6, 8, 10]. Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
+
+// const numbers = [2, 4, 6, 8, 10];
+
+// const checkNumber = numbers => numbers.every(el => !(el % 2));
+
+// console.log(checkNumber(numbers));
+
+
+// Знайдіть перше непарне число
+
+// const numbers = [2, 1, 6, 8, 9, 10, 12];
+
+// const findOddNUmber = numbers => numbers.find(el => el % 2);
+
+// console.log(findOddNUmber(numbers));
+
+// Відсортуйте масив чисел [4, 2, 5, 1, 3] у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+// const sortNumbers = array => array.toSorted((a, b) => a - b) 
+
+// console.log(sortNumbers([4, 2, 5, 1, 3]));
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [16, 4, 25, 1, 9];
+
+const sameArray = (array1, array2) => {
+  if (array1.length !== array2.length) return false;
+  const array3 = array1.map(el => Math.pow(el, 2)).toSorted((a, b) => a - b);
+  const array4 = array2.toSorted((a, b) => a - b);
+
+  return array3.every((el, indx) => el === array4[indx])
 }
 
-console.log(findNar(people));
-
-  
+console.log(sameArray(array1, array2)); 
