55e6830c05973e33b6368726019f6cb9b935b9b6
BakelD <bakeld85@gmail.com>
2025-02-28 14:56:52 +0100

    Add 4 tasks

--- js/module-07/lesson-07-02.js
+++ js/module-07/lesson-07-02.js
@@ -17,32 +17,32 @@
    \ При відправці форми, очисти інпут, верни чек бокс у положення false, верни дефолтне значення "Anonymous" у span.
  */
 
-const refs = {
-  input: document.querySelector('.js-username-input'),
-  form: document.querySelector('.js-contact-form'),
-  userName: document.querySelector('.js-username-output'),
-};
+// const refs = {
+//   input: document.querySelector('.js-username-input'),
+//   form: document.querySelector('.js-contact-form'),
+//   userName: document.querySelector('.js-username-output'),
+// };
 
-const hadleInputValidation = function () {
-  // refs.userName.textContent = refs.input.value ? refs.input.value : 'Anonimous';
-  refs.userName.textContent = refs.input.value || 'Anonimous';
-};
-const handleSubmit = function (e) {
-  e.preventDefault();
+// const hadleInputValidation = function () {
+//   // refs.userName.textContent = refs.input.value ? refs.input.value : 'Anonimous';
+//   refs.userName.textContent = refs.input.value || 'Anonimous';
+// };
+// const handleSubmit = function (e) {
+//   e.preventDefault();
 
-  const { accept, userName } = e.target.elements;
-  if (!userName.value || !accept.checked) {
-    alert('Not valid data');
-    return;
-  }
-  const userdata = { name: userName.value };
-  console.log(userdata);
-  refs.form.reset();
-  refs.userName.textContent = 'Anonimous';
-};
+//   const { accept, userName } = e.target.elements;
+//   if (!userName.value || !accept.checked) {
+//     alert('Not valid data');
+//     return;
+//   }
+//   const userdata = { name: userName.value };
+//   console.log(userdata);
+//   refs.form.reset();
+//   refs.userName.textContent = 'Anonimous';
+// };
 
-refs.input.addEventListener('input', hadleInputValidation);
-refs.form.addEventListener('submit', handleSubmit);
+// refs.input.addEventListener('input', hadleInputValidation);
+// refs.form.addEventListener('submit', handleSubmit);
 
 // const handleInput = function (e) {
 //   // refs.input.classList.add('error');
@@ -78,3 +78,103 @@ refs.form.addEventListener('submit', handleSubmit);
 //   }
 // };
 // refs.input.addEventListener('blur', handleBlurInput);
+
+// ------------------------------------------------------------
+
+// Кнопка "Приховати" ховає текст і замінює назву кнопки на "Розкрити", у разі повторного натискання текст знову стає доступним і кнопка набуває початкового вигляду.
+
+// const refs = {
+//   input: document.querySelector('.js-password-input'),
+//   btn: document.querySelector('.js-password-button'),
+// };
+
+// refs.btn.addEventListener('click', e => {
+//   if (refs.input.type === 'text') {
+//     refs.input.type = 'password';
+//     e.target.textContent = 'Розкрити';
+//   } else {
+//     refs.input.type = 'text';
+//     e.target.textContent = 'Приховати';
+//   }
+// });
+
+// ------------------------------------------------------------
+
+// Написати функцію, яка буде створювати список подій клавіатури event.key та event.code
+// Додати класи на список eventList, на елементи eventCode та eventKey
+
+// const refs = {
+//   listThumb: document.querySelector('.eventThumb'),
+// };
+
+// document.addEventListener('keydown', e => {
+//   const markup = `<li class="eventKey">key -> ${e.key}</li>
+//   <li class="eventCode">code -> ${e.code}</li>`;
+
+//   refs.listThumb.insertAdjacentHTML('beforeend', markup);
+
+// });
+
+// ------------------------------------------------------------
+
+// Кнопка "Зменшити" робить квадрат меншим на 20 пікселів, кнопка "Збільшити" - робить більшим на 10 пікселів.
+
+// const refs = {
+//   box: document.querySelector('.box'),
+//   btnDecr: document.querySelector('.js-decrease'),
+//   btnIncr: document.querySelector('.js-increase'),
+// };
+
+// let boxWidth = refs.box.offsetWidth;
+
+// const onBtnDecr = () => {
+//   boxWidth -= 20;
+
+//   refs.box.style.width = `${boxWidth}px`;
+//   refs.box.style.height = `${boxWidth}px`;
+// };
+
+// const onBtnIncr = () => {
+//   boxWidth += 10;
+
+//   refs.box.style.width = `${boxWidth}px`;
+//   refs.box.style.height = `${boxWidth}px`;
+// };
+
+// refs.btnDecr.addEventListener('click', onBtnDecr);
+// refs.btnIncr.addEventListener('click', onBtnIncr);
+
+// const onBoxChange = e => {
+//   const { offsetWidth } = refs.box;
+//   const { id } = e.target;
+
+//   refs.box.style.width = `${offsetWidth + (id === 'increase' ? 10 : -20)}px`;
+//   refs.box.style.height = `${offsetWidth + (id === 'increase' ? 10 : -20)}px`;
+// };
+
+// refs.btnDecr.addEventListener('click', onBoxChange);
+// refs.btnIncr.addEventListener('click', onBoxChange);
+// =============================================================
+
+//При натисканні на кола вони змінюють колір.
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  outerCircle: document.querySelector('.outer-circle'),
+  innerCircle: document.querySelector('.inner-circle'),
+};
+
+// refs.outerCircle.addEventListener('click', e => {
+//   e.target.style.backgroundColor = getRandomColor();
+// });
+
+refs.outerCircle.addEventListener('click', e => {
+  if (e.target === e.currentTarget) {
+    e.target.style.borderColor = getRandomColor();
+    return;
+  }
+
+  e.target.style.backgroundColor = getRandomColor();
+});
